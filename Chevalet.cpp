306ef40a3c3c9c9a6ddfdfd5e090ddc29fb15d5d
Pierre Lemétayer <e127718@etu.univ-nantes.fr>
2015-10-13 17:43:00 +0200

    Début pattern factory tuile

---
+++ Chevalet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>	
+#include <vector>
+
+using namespace std;
+
+class Chevalet
+{
+	private:
+	
+	vector<Tuile> listeTuile_; 
+	
+	public:
+	
+	Chevalet();
+};
---
+++ Tuile.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+class Tuile
+{
+	public:
+	
+	virtual string getCouleur();
+	virtual int getValeur();
+	virtual int getId();
+};
